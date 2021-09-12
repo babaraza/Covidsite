d2f29fb5ee1380e2d82b6308d4e09d4121313c34
babar <syedrazatx@gmail.com>
2021-09-12 17:54:15 -0500

    changing django debug to false

--- crap/models.py
+++ crap/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CovidData(models.Model):
     total_deaths = models.CharField(max_length=20)
     total_infections = models.CharField(max_length=20)
-    last_updated = models.CharField(max_length=50)
+    last_updated = models.CharField(max_length=60)
 
     def __str__(self):
         return f'☠️ {self.total_deaths}'
